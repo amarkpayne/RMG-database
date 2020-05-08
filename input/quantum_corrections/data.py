a307dbeb8b016bca7036925c9ffacf108ebf9dfb
Mark Payne <ampayne@mit.edu>
2020-05-08 12:34:34 -0400

    Add atom energies for dlpno-ccsd(t)-f12/cc-pvdz-f12
    uHF dlpno-ccsd(t)-f12 cc-pvdz-f12 aug-cc-pvdz/c cc-pvdz-f12-cabs  TightSCF tightPNO

--- input/quantum_corrections/data.py
+++ input/quantum_corrections/data.py
@@ -338,6 +338,14 @@ atom_energies = {
         'O': -74.97847534 + SOC['O'], 'S': -397.6571654 + SOC['S']
     },
 
+    # Calculated using single atom energies. Full model chemistry is:
+    # uHF dlpno-ccsd(t)-f12 cc-pvdz-f12 aug-cc-pvdz/c cc-pvdz-f12-cabs  TightSCF tightPNO
+    'dlpno-ccsd(t)-f12/cc-pvdz-f12': {
+        'H': -0.49980981 + SOC['H'], 'C': -37.783858494233 + SOC['C'], 'N': -54.521290948316 + SOC['N'],
+        'O': -74.987793987468 + SOC['O'], 'S': -397.654801621994 + SOC['S'], 'Cl': -459.677284014605 + SOC['Cl'],
+        'F': -99.643299597155 + SOC['F'],
+    },
+
 }
 
 # Petersson-type bond additivity correction parameters
